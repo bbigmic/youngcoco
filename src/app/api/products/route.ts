86c3cc048296ad36a3fdfbbbcb2f4a84a754b96b
bbigmic <122184799+bbigmic@users.noreply.github.com>
2025-07-15 18:43:53 +0200

    fix5 delete product route

--- src/app/api/products/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export async function GET() {
-  const products = await prisma.product.findMany();
-  return NextResponse.json(products);
-} 
